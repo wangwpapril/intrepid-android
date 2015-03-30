59df2d26026cb3752fde1055a1eefbfb834d70f0
jcarrharris <joncarrharris@gmail.com>
2015-03-30 13:28:09 -0400

    project merge completed

--- app/src/main/java/com/swishlabs/intrepid_android/activity/MainActivity.java
+++ app/src/main/java/com/swishlabs/intrepid_android/activity/MainActivity.java
@@ -1,9 +1,11 @@
-package com.swishlabs.intrepid_android;
+package com.swishlabs.intrepid_android.activity;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.swishlabs.intrepid_android.R;
 
 
 public class MainActivity extends ActionBarActivity {
