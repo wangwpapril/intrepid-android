4f6d7cb4e334dea8f07fb6b0149a0c9f39a22f05
rraciop <ryanracioppo@gmail.com>
2015-04-10 16:24:09 -0400

    remove tabs

--- app/src/main/java/com/swishlabs/intrepid_android/activity/ViewDestinationActivity.java
+++ app/src/main/java/com/swishlabs/intrepid_android/activity/ViewDestinationActivity.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -57,30 +55,6 @@ public class ViewDestinationActivity extends ActionBarActivity {
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
 
-        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        actionBar.setHomeButtonEnabled(false);
-        actionBar.setNavigationMode(android.support.v7.app.ActionBar.NAVIGATION_MODE_TABS);
-
-        // Adding Tabs
-        for (String tab_name : tabs) {
-            actionBar.addTab(actionBar.newTab().setText(tab_name).setTabListener(new ActionBar.TabListener() {
-                @Override
-                public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-
-                }
-
-                @Override
-                public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-
-                }
-
-                @Override
-                public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-
-                }
-            }));
-
-        }
 
     }
 
