e997d1c6057e84576fc28033d032e3fc00552696
Jon Carr-Harris <joncarrharris@gmail.com>
2015-04-21 16:27:16 -0400

    further fixes to menu

--- app/src/main/java/com/swishlabs/intrepid_android/customViews/IntrepidMenu.java
+++ app/src/main/java/com/swishlabs/intrepid_android/customViews/IntrepidMenu.java
@@ -30,7 +30,7 @@ import com.swishlabs.intrepid_android.activity.ViewHealthActivity;
 
 public class IntrepidMenu extends ScrollView {
 
-    public static final int MENUHEIGHT = 325;
+    public static final int MENUHEIGHT = 328;
     public static final int MINHEIGHT = 35;
     public static final int VELOCITY = 300;
     private int mInitialHeight =0;
