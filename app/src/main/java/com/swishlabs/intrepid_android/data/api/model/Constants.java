043cedeae2b1bf17a953d5e5907622867c566792
Will Wang <wwang@peakcontact.com>
2015-04-21 12:57:42 -0400

    weather base

--- app/src/main/java/com/swishlabs/intrepid_android/data/api/model/Constants.java
+++ app/src/main/java/com/swishlabs/intrepid_android/data/api/model/Constants.java
@@ -5,10 +5,11 @@ package com.swishlabs.intrepid_android.data.api.model;
  */
 public class Constants {
 // production API:
-public final static String BASE_URL = "https://staging.intrepid247.com/v1/";
-public final static String CURRENCY_URL = "https://openexchangerates.org/api/latest.json?app_id=14073d8e6b8c4687951ed926cbbd3589";
-String login = "cshah3@alumni.uwo.ca";
-String pw = "iloveapple";
+    public final static String BASE_URL = "https://staging.intrepid247.com/v1/";
+    public final static String CURRENCY_URL = "https://openexchangerates.org/api/latest.json?app_id=14073d8e6b8c4687951ed926cbbd3589";
+    public static final String WEATHER_URL = "https://m.intrepid247.com/weather.html";
+    String login = "cshah3@alumni.uwo.ca";
+    String pw = "iloveapple";
 // development API:
 //   public final static String BASE_URL = "http://devlamp03/intrepid-api-laravel/public/v1";
 }
