9fc35e01b746e356a4d91129a4a9067a906e07dd
Will Wang <wwang@peakcontact.com>
2015-04-30 14:03:08 -0400

    clickable indicator spot

--- app/src/main/java/com/swishlabs/intrepid_android/customViews/IndicatorLinearLayout.java
+++ app/src/main/java/com/swishlabs/intrepid_android/customViews/IndicatorLinearLayout.java
@@ -2,7 +2,11 @@
 package com.swishlabs.intrepid_android.customViews;
 
 import android.content.Context;
+import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -17,6 +21,7 @@ public class IndicatorLinearLayout extends LinearLayout {
     private List<ImageView> points = new ArrayList<ImageView>();
     private Context context;
     private int selectedIndex;
+    private ViewPager mViewPager;
 
     public IndicatorLinearLayout(Context context) {
         super(context);
@@ -28,19 +33,32 @@ public class IndicatorLinearLayout extends LinearLayout {
         this.context = context;
     }
 
-    public void initPoints(int count, int selectedIndex) {
+    public void initPoints(int count, int selectedIndex, ViewPager viewPager) {
     	this.removeAllViews();
         this.selectedIndex = selectedIndex;
+        this.mViewPager = viewPager;
+
         for (int i = 0; i < count; i++) {
             ImageView point = new ImageView(context);
             point.setImageResource(R.drawable.home_indirector_point);
             if (i == selectedIndex) {
-                point.setEnabled(true);
+                point.setSelected(true);
             } else {
-                point.setEnabled(false);
+                point.setSelected(false);
             }
+            point.setTag(i);
+
             points.add(i, point);
-            LayoutParams params = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            point.setOnClickListener( new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int index = (int) v.getTag();
+                    indicator(index);
+                    mViewPager.setCurrentItem(index);
+                }
+            });
+
+            LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             params.leftMargin = 15;
             this.addView(points.get(i),params);
         }
@@ -48,10 +66,28 @@ public class IndicatorLinearLayout extends LinearLayout {
     }
 
     public void indicator(int nextSelectedIndex) {
-        points.get(selectedIndex).setEnabled(false);
-        points.get(nextSelectedIndex).setEnabled(true);
+        points.get(selectedIndex).setSelected(false);
+        points.get(nextSelectedIndex).setSelected(true);
         this.selectedIndex = nextSelectedIndex;
         invalidate();
     }
+
+/*    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }*/
+
+/*    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return false;
+    }*/
+
+/*    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        onTouchEvent(ev);
+//        return super.dispatchTouchEvent(ev);
+        return false;
+    }*/
+
 
 }
