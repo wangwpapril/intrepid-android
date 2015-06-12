d113a7d70de839bd7fc31bb90ff73f0effb4bd47
Will Wang <wwang@peakcontact.com>
2015-06-12 16:34:33 -0400

    fix to embassy view option menu

--- app/src/main/java/com/swishlabs/intrepid_android/activity/ViewEmbassyActivity.java
+++ app/src/main/java/com/swishlabs/intrepid_android/activity/ViewEmbassyActivity.java
@@ -145,7 +145,7 @@ public class ViewEmbassyActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_view_embassy, menu);
+//        getMenuInflater().inflate(R.menu.menu_view_embassy, menu);
         return true;
     }
 
